7281415fbad602065770a317595b26c8fac0709f
jack <jack.gammon@equinox.co.nz>
2016-08-05 14:47:57 +1200

    Revert "Added test environment" Changed my mind :) This reverts commit 5757f163e25f22d6ba671516ece9d730ceb59f11.

--- public_html/tests/logicTests.js
+++
@@ -1,9 +0,0 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
-});
